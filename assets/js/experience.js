8da08259bdb11601d27b7ef50a69591eea07c66e
supershivam13 <shivam_1901cs55@iitp.ac.in>
2021-05-27 23:54:18 +0530

    updated

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -4,25 +4,39 @@
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Secretary, Students' Technical Council",
+    cardImage: "assets/images/experience-page/gym.png",
+    place: "Student's Gymkhana, IIT Patna",
+    time: "(Feb. 2021 - present)",
+    desp: "<li>An elected representative providing for technical, administrative, and clerical support associated with all inter-disciplinary technical activities involving the institute.</li> <li>Established the GeeksforGeeks Student Chapter at IIT Patna</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Microsoft Learn Student Ambassador - Beta",
+    cardImage: "assets/images/experience-page/beta.png",
+    place: "Microsoft",
+    time: "(Apr. 2021 - Present)",
+    desp: "<li>Student Ambassadors is a community of students who are eager to help students and their communities, lead in their local tech circles, and develop technical and career skills for the future.</li><li>Conducted an event 'Hosting your First Web App on Streamlit with VS Code' with around 50 attendees.</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Sub-Cord, Web and App Committee",
+    cardImage: "assets/images/experience-page/inf.jpg",
+    place: "Infinito, IIT Patna",
+    time: "(Mar. 2020 - May 2021)",
+    desp:"<li>Responsible for the development and maintenance the website and application of Infinito - Sports Fest of IIT Patna.</li><li>Mentored juniors to learn Open Source by contributing to the website.</li>",
+  },
+  {
+    title: "Google Crowdsource Influencer",
+    cardImage: "assets/images/experience-page/crowd.png",
+    place: "Google Crowdsource",
+    time: "(Feb. 2021 - Present)",
+    desp:"<li>CrowdSource Influencers are the representative of Crowdsource community for their region/campus encouraging peers to contribute, enabling diversity in Machine Learning models and make Google products work equally well for everyone.</li><li>Got Featured as a 'Top Contributor' on the Google Crowdsource Offical Facebook page.</li>",
+  },
+  {
+    title: "Sub-Cord, Hospitality Committee",
+    cardImage: "assets/images/experience-page/cel.jpg",
+    place: "Celesta, IIT Patna",
+    time: "(Mar. 2020 - May 2021)",
+    desp:"<li>Responsible for the arrangements and proper hospitality to the Chief Guests, Event Speakers, VIPs, etc at Celesta - Technical Fest of IIT Patna.</li><li>Look after the proper management of transportation servies of Chief Guets, VIPs, etc to and from the campus.</li>",
   },
 ];
 
@@ -62,41 +76,41 @@ document.addEventListener("DOMContentLoaded", showCards2);
 const volunteership = document.querySelector(".volunteership");
 const volunteershipcards = [
   {
-    title: "GirlScript Summer Of Code 2020",
+    title: "Community Member, GDG Patna",
     cardImage: "assets/images/experience-page/1.jpg",
     description:
-      "Responsible for handling the projects GirlScript App and GirlScript Website Boilerplate.",
+      "GDG Patna is a local group of developers who are specifically interested in Google products and APIs",
   },
   {
-    title: "StudentCode-in 2020",
+    title: "Contingent Member, 9th Inter IIT Tech Meet",
     cardImage: "assets/images/experience-page/2.jpg",
     description:
-      "Responsible for handling open source contributions for the project Awesome Developer Portfolio.",
+      "Analysed the existing problems and proposed solutions in the event 'Product Case Study'",
   },
   {
-    title: "PClub Summer Of Code 2020",
+    title: "Contingent Member, 8th Inter IIT Tech Meet",
     cardImage: "assets/images/experience-page/3.jpg",
     description:
-      "Mentoring for the projects Deep Pixel, AutoVaidya, Just Resume and Doc2pen.",
+      "Presented the project 'DigiMonitor' of IIT Patna in the event 'Engineer's Conclave'",
   },
-  {
-    title: "Hakin-Codes",
-    cardImage: "assets/images/experience-page/4.jpg",
-    description:
-      "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
-  },
-  {
-    title: "30 DaysofFlutter",
-    cardImage: "assets/images/experience-page/5.jpg",
-    description:
-      "Helping beginners from their first steps in Flutter to building fully functional cross-platform applications.",
-  },
-  {
-    title: "Garuda Hacks",
-    cardImage: "assets/images/experience-page/6.jpg",
-    description:
-      "MLH sponsored Indonesia's premier Global Virtual Hackathon on 14 - 16th August 2020.",
-  },
+  // {
+  //   title: "Hakin-Codes",
+  //   cardImage: "assets/images/experience-page/4.jpg",
+  //   description:
+  //     "Mentoring for the open source projects Deeppixel, Awesome Developer Portfolios and Doc2Pen.",
+  // },
+  // {
+  //   title: "30 DaysofFlutter",
+  //   cardImage: "assets/images/experience-page/5.jpg",
+  //   description:
+  //     "Helping beginners from their first steps in Flutter to building fully functional cross-platform applications.",
+  // },
+  // {
+  //   title: "Garuda Hacks",
+  //   cardImage: "assets/images/experience-page/6.jpg",
+  //   description:
+  //     "MLH sponsored Indonesia's premier Global Virtual Hackathon on 14 - 16th August 2020.",
+  // },
 ];
 
 const showCards = () => {
@@ -124,18 +138,18 @@ document.addEventListener("DOMContentLoaded", showCards);
 
 const mentorshipcards = document.querySelector(".mentorship-cards");
 const mentor = [
-  {
-    title: "HakinCode",
-    image: "assets/images/experience-page/hakin.png",
-    time: "06/2020 - 08/2020",
-    desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
-  },
-  {
-    title: "Google Summer of Code",
-    image: "assets/images/experience-page/gsoc.png",
-    time: "03/2020 - 08/2020",
-    desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
-  },
+  // {
+  //   title: "HakinCode",
+  //   image: "assets/images/experience-page/hakin.png",
+  //   time: "06/2020 - 08/2020",
+  //   desp: "<li>It is an open source community where students and mentors can apply.</li><hr /><li>Ample amount of technologies and projects are there and we are given opportunity to work on them according to our interest and knowledge.</li>",
+  // },
+  // {
+  //   title: "Google Summer of Code",
+  //   image: "assets/images/experience-page/gsoc.png",
+  //   time: "03/2020 - 08/2020",
+  //   desp: "<li>Google Summer of Code is a global program focused on introducing students to open source software development.</li><hr /><li>It is a great platform to explore new areas, maybe discover a new career path!</li>",
+  // },
 ];
 
 const showCards3 = () => {
